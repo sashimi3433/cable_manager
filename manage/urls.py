fadc217934d14d29ea3c624aa7bea544abde20be
酒井 風志 <100179630+sashimi3433@users.noreply.github.com>
2025-01-23 00:39:47 +0900

    データの編集機能を追加

--- manage/urls.py
+++ manage/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('create', views.create, name='create'),
     path('new', views.new, name='new'),
     path('<int:id>/delete', views.delete, name='delete'),
+    path('<int:id>/edit', views.edit, name='edit'),
+    path('<int:id>/update', views.update, name = 'update'),
+
 ]
--- manage/views.py
+++ manage/views.py
@@ -54,3 +54,32 @@ def create(request):
     else:
         regForm = RegForm()
     return render(request, 'manage/add.html', {'regForm': regForm})
+
+@login_required
+def edit(request, id):
+
+    item = get_object_or_404(Item, pk=id)
+    regForm = RegForm(instance=item)
+    context = {
+        'message': 'ページID:'+ str(id) +"を編集中",
+        'item': item,
+        'regForm': regForm,
+    }
+    return render(request, 'manage/edit.html', context)
+
+def update(request, id):
+    if request.method == 'POST':
+        item = get_object_or_404(Item, pk=id)
+        regForm = RegForm(request.POST, instance=item)
+        if regForm.is_valid():
+            item.save()
+            return redirect('manage:detail', id=item.id)
+    else:
+        return redirect('manage:edit', id=id)
+
+    context = {
+        'message': '更新に失敗しました',
+        'item': item,
+        'regForm': regForm,
+    }
+    return render(request, 'manage/edit.html', context)
